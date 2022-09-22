06b0cc15af5ab32494d2a1900a5dd7fd07ea6016
Kaung L <kaung.lin@gmail.com>
2022-09-22 16:55:04 +0700

    Fix typo

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from functools import partial
-from downloaders import HttpDownloader, SFTPDownloader
+from downloaders import HttpDownloader, SFTPDownloader, FTPDownloader
 
 DEFAULT_DIR="/tmp"
 DEFAULT_RETRY=3
 DOWNLOADERS = {
     "http": partial(HttpDownloader, retries=DEFAULT_RETRY),
     "https": partial(HttpDownloader, retries=DEFAULT_RETRY),
-    "ftp": SFTPDownloader,
-    "sftp": SFTPDownloader,
+    "ftp": FTPDownloader,
+    "sftp": SFTPDownloader
 }
