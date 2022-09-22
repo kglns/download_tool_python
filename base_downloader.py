bd9574932ab0c3948f2f47360f1ec95d6c4d500a
Kaung L <kaung.lin@gmail.com>
2022-09-22 16:53:22 +0700

    Add remove_file method

--- base_downloader.py
+++ base_downloader.py
@@ -18,6 +18,10 @@ class BaseDownloader:
 
         return "{0}/{1}".format(self.default_dir, filename)
 
+    def remove_file(self, filename):
+        if os.path.isfile(filename):
+            print("Removing file: {0}".format(filename))
+            os.remove(filename)
     '''
     Prints any error during download, and remove file if exists
     '''
@@ -25,20 +29,22 @@ class BaseDownloader:
         print("----Error occoured during downloading----")
         print(error)
 
-        if os.path.isfile(filename):
-            print("Removing file: {0}".format(filename))
-            os.remove(filename)
+        self.remove_file(filename)
 
     '''
-    This method must be implemented by child classes
+    This method must be implemented by child classes. Other key, value params can be added as needed.
     '''
     def download(self, uri, filename, **kwargs):
         raise NotImplementedError
 
+    '''
+    This takes care of the actual run - it also handles any uncaught exceptions and cleans up.
+    '''
     def run(self, uri, **kwargs):
         try:
             filename = self.generate_filename(uri)
-            self.download(uri, filename)
+            self.remove_file(filename) # if a file already exits, remove it
+            self.download(uri, filename, **kwargs)
         # Catch any unhandled exceptions and log here. Specific exceptions can be handled in individual Downloader as well.
         except Exception as e:
             self.clean_up(e, filename)
