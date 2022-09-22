55b87459910708eecfa0cb97e33089b4fa597155
Kaung L <kaung.lin@gmail.com>
2022-09-22 16:01:58 +0700

    Add FTPDownloader and modify extract methods

--- downloaders.py
+++ downloaders.py
@@ -2,6 +2,7 @@ import re
 import urllib3
 import shutil
 import paramiko
+from ftplib import FTP
 from urllib3.util import Retry
 from base_downloader import BaseDownloader
 from paramiko.ssh_exception import AuthenticationException, ChannelException, SSHException
@@ -32,7 +33,11 @@ class SFTPDownloader(BaseDownloader):
         self.port = port
 
     def _extract_credentials(self, uri):
-        # sftp://user:password@example.com:/test/file -> user
+        # If there is no @ sign, no user/pw are given
+        if not '@' in uri:
+            return None, None
+        # Otherwise, extract per below
+        # sftp://user:password@example.com:/test/file -> user, password
         creds = uri.split("//")[1].split("@")[0]
         if creds:
             user, password = creds.split(":")
@@ -41,20 +46,30 @@ class SFTPDownloader(BaseDownloader):
 
     def _extract_hostname(self, uri):
         # sftp://user:password@example.com:/test/file -> example.com
-        return uri.split("@")[1].split(":")[0]
+        if not "@" in uri:
+            cleaned_uri = uri.split("//")[1]
+            hostname = cleaned_uri.split(":")[0]
+        else:
+            hostname = uri.split("@")[1].split(":")[0]
+        return hostname
 
     def _extract_remote_path(self, uri):
         # sftp://user:password@example.com:/test/file -> /test/file
         return uri.split(":")[-1]
 
     def generate_filename(self, uri):
-        cleaned_uri = uri.split("@")[1]
+        # Sometimes we don't get username:password@hostname, so need to split by //
+        if not "@" in uri:
+            cleaned_uri = uri.split("//")[1]
+        else:
+            cleaned_uri = uri.split("@")[1]
         filename = re.sub(r"(\/|:)", "-", cleaned_uri)
-        return filename
+        filename = re.sub(r"(-)\1+", "\1", filename) # remove annoying '--' in filename
+        return "{0}/{1}".format(self.default_dir, filename)
 
     def download(self, uri, filename):
 
-        # Extract particulars from URI
+        # Extract conn. info from URI
         user, password = self._extract_credentials(uri)
         host = self._extract_hostname(uri)
         remote_path = self._extract_remote_path(uri)
@@ -84,6 +99,33 @@ class SFTPDownloader(BaseDownloader):
                 transport.close()
 
 class FTPDownloader(SFTPDownloader):
-    pass
+    
+    def __init__(self, default_dir='/tmp'):
+        super(FTPDownloader, self).__init__(default_dir)
+    
+    def download(self, uri, filename):
+        
+        # Extract conn. info from URI
+        # Use all extract methods from parent class
+        user, password = self._extract_credentials(uri)
+        host = self._extract_hostname(uri)
+        remote_path = self._extract_remote_path(uri)
+
+        # Init FTP
+        ftp = FTP(host)
+        try:
+            if user and password:
+                ftp.login(user, password)
+            else:
+                ftp.login()
+
+            with open(filename, 'wb') as fp:
+                ftp.retrbinary('RETR {0}'.format(remote_path), fp.write)
+            print("----Download successful for {0}----".format(uri))
+            print("----File saved at {0}----".format(filename))
+        except Exception as e:
+            self.clean_up(e, filename)
+        finally: # Ensure all connections are closed
+            ftp.close()
 
 
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import paramiko
 import unittest
 from base_downloader import BaseDownloader
-from downloaders import HttpDownloader, SFTPDownloader
+from downloaders import FTPDownloader, HttpDownloader, SFTPDownloader
 
 class TestDownloaders(unittest.TestCase):
 
@@ -11,13 +11,15 @@ class TestDownloaders(unittest.TestCase):
         self.bad_uri = 'https://zipdia.com/test/file'
         self.good_uri = 'https://ftp.gnu.org/old-gnu/Manuals/binutils-2.12/html_mono/binutils.html'
 
-        self.sftp_uri = 'ftp://sftpuser:password@kaung.dev:/test/hello.txt'
+        self.sftp_uri = 'ftp://sftpuser:UbSftp^1@kaung.dev:/test/hello.txt'
 
         self.base_downloader = BaseDownloader()
         self.http = HttpDownloader()
     
         # key = paramiko.RSAKey.from_private_key_file("/path/to/secret")
         self.sftp = SFTPDownloader()
+
+        self.ftp = FTPDownloader()
 
     # BaseClass tests
     def test_generate_filename(self):
@@ -31,20 +33,23 @@ class TestDownloaders(unittest.TestCase):
         self.http.run(self.bad_uri)
 
     # FTP/SFTP tests
-    # def test_sftp_download_get_creds(self):
-    #     self.assertEqual(self.sftp._extract_credentials(self.sftp_uri[0]), ('user', 'pw'))
+    def test_sftp_download_get_creds(self):
+        self.assertEqual(self.sftp._extract_credentials(self.sftp_uri[0]), ('user', 'pw'))
 
-    # def test_sftp_download_get_hostname(self):
-    #     self.assertEqual(self.sftp._extract_hostname(self.sftp_uri[0]), 'example.com')
+    def test_sftp_download_get_hostname(self):
+        self.assertEqual(self.sftp._extract_hostname(self.sftp_uri[0]), 'example.com')
     
-    # def test_sftp_download_get_remote_path(self):
-    #     self.assertEqual(self.sftp._extract_remote_path(self.sftp_uri[0]), '/file')
+    def test_sftp_download_get_remote_path(self):
+        self.assertEqual(self.sftp._extract_remote_path(self.sftp_uri[0]), '/file')
     
-    # def test_sftp_download_generate_filename(self):
-    #     self.assertEqual(self.sftp.generate_filename(self.sftp_uri[0]), 'example.com--file')
+    def test_sftp_download_generate_filename(self):
+        self.assertEqual(self.sftp.generate_filename(self.sftp_uri[0]), 'example.com--file')
 
-    # def test_sftp_download_bad_auth_expected(self):
-    #     self.sftp.run(self.sftp_uri)
+    def test_sftp_download_bad_auth_expected(self):
+        self.sftp.run(self.sftp_uri)
+
+    def test_ftp_download_from_debian_succeed(self):
+        self.ftp.run("ftp://ftp.us.debian.org:/debian/README")
 
 if __name__ == '__main__':
     unittest.main()
