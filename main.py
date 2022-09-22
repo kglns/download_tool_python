82c283ca04c79d2ee5859eee4410e2332ba853db
Kaung L <kaung.lin@gmail.com>
2022-09-22 14:56:06 +0700

    Clean up main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import argparse
-from config import DOWNLOADERS
+from config import DOWNLOADERS, DEFAULT_DIR
 from utils import extract_protocol
 
-parser = argparse.ArgumentParser(description='Download files')
+parser = argparse.ArgumentParser(description='Utility tool for downloading files')
 parser.add_argument("--uri")
 args = parser.parse_args()
+uri = args.uri
 
 def main():
-    protocol = extract_protocol(args.uri)
+    protocol = extract_protocol(uri)
     downloader = DOWNLOADERS.get(protocol)
 
     if downloader:
-        downloader.run(args.uri)
+        print("Using {0} to download".format(downloader.__name__))
+        executor = downloader(default_dir=DEFAULT_DIR)
+        executor.run(uri)
     else:
         print("Given protocol is not supported: {0}".format(protocol))
 
