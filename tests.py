4e8254a5b1e91688f7efcee7ca40b784d89964db
Kaung L <kaung.lin@gmail.com>
2022-09-22 16:34:51 +0700

    Updated uris

--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@ class TestDownloaders(unittest.TestCase):
         self.bad_uri = 'https://zipdia.com/test/file'
         self.good_uri = 'https://ftp.gnu.org/old-gnu/Manuals/binutils-2.12/html_mono/binutils.html'
 
-        self.sftp_uri = 'ftp://sftpuser:UbSftp^1@kaung.dev:/test/hello.txt'
+        self.sftp_uri = 'ftp://user:pw@kaung.dev:/test/hello.txt'
 
         self.base_downloader = BaseDownloader()
         self.http = HttpDownloader()
@@ -34,16 +34,16 @@ class TestDownloaders(unittest.TestCase):
 
     # FTP/SFTP tests
     def test_sftp_download_get_creds(self):
-        self.assertEqual(self.sftp._extract_credentials(self.sftp_uri[0]), ('user', 'pw'))
+        self.assertEqual(self.sftp._extract_credentials(self.sftp_uri), ('user', 'pw'))
 
     def test_sftp_download_get_hostname(self):
-        self.assertEqual(self.sftp._extract_hostname(self.sftp_uri[0]), 'example.com')
+        self.assertEqual(self.sftp._extract_hostname(self.sftp_uri), 'kaung.dev')
     
     def test_sftp_download_get_remote_path(self):
-        self.assertEqual(self.sftp._extract_remote_path(self.sftp_uri[0]), '/file')
+        self.assertEqual(self.sftp._extract_remote_path(self.sftp_uri), '/test/hello.txt')
     
     def test_sftp_download_generate_filename(self):
-        self.assertEqual(self.sftp.generate_filename(self.sftp_uri[0]), 'example.com--file')
+        self.assertEqual(self.sftp.generate_filename(self.sftp_uri), '/tmp/kaung.devtest-hello.txt')
 
     def test_sftp_download_bad_auth_expected(self):
         self.sftp.run(self.sftp_uri)
